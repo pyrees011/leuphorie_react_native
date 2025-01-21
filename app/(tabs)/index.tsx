f670f928d05bfccd7709fa435fa7b07e2707487f
Pyrees011 <raunak.bhansali17@gmail.com>
2025-01-21 23:40:15 +0100

    feat(questionnaire): adds the questionnaire + stack layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,14 @@
-import { Text, StyleSheet, StatusBar } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
+import { router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <TouchableOpacity onPress={() => router.push('/questionnaire')}>
+        <Text>Go to questionnaire</Text>
+      </TouchableOpacity>
       <Text className="text-2xl font-bold">Initializing...</Text>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
